ab37f9d2dacc9e962ff99b740eca3d9e375b2364
takashikawanaka <takashi.soccer.a@gmail.com>
2023-02-28 18:32:21 +0900

    add Chapter9

--- src/main/java/com/techacademy/controller/IndexController.java
+++ src/main/java/com/techacademy/controller/IndexController.java
@@ -1,14 +1,26 @@
 package com.techacademy.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.techacademy.service.ReportService;
+import com.techacademy.service.UserDetail;
+
 @Controller
 public class IndexController {
+    private final ReportService reportService;
+
+    public IndexController(ReportService service) {
+        this.reportService = service;
+    }
 
     @GetMapping("/")
-    public String index() {
+    public String index(@AuthenticationPrincipal UserDetail userDetail, Model model) {
+        model.addAttribute("username", userDetail.getEmployee().getName());
+        model.addAttribute("reportlist", reportService.getReportListByEmployee(userDetail.getEmployee()));
         return "index";
     }
 
